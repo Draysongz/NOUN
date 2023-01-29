13f8c3ebcac66762c79fb44f1980a8bdaeac9132
Ackermann <dharmiegra4@gmail.com>
2023-01-29 14:53:09 +0100

    newly

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Dash = () => {
   return (
-    <div>Dash</div>
+    <div>
+        <div style="background: darkgreen; border: 1px solid white; /* linear-gradient(to right,  green, darkgreen, green) */
+border-radius: 1px;">
+<center>
+    <h5> 
+        <strong>
+            <p style="color: white">NATIONAL OPEN UNIVERSITY OF NIGERIA</p>
+            </strong> 
+            </h5>
+  <div className="container-fluid" style="background-color: light-grey">
+    <p id="red"><font color="wheat"></font></p><h5><font color="wheat">
+      University Village,
+      Plot 91, Cadastral Zone,
+      Nnamdi Azikiwe Express Way
+      Jabi-Abuja. </font></h5><p></p></div></center>
+    </div>
+    </div>
   )
 }
 
