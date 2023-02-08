5bca14abc27a2f18fb410b728db00e35dd97477a
Ackermann <dharmiegra4@gmail.com>
2023-02-08 19:10:30 +0100

    new

--- src/Result.jsx
+++ src/Result.jsx
@@ -3,12 +3,15 @@ import './result.css'
 // import jsPDF from 'jspdf';
 // import html2canvas from 'html2canvas';
 import logo from './assets/icon.webp'
-import { PDFExport } from "@progress/kendo-react-pdf";
-
+import { PDFExport } from '@progress/kendo-react-pdf'
 
 
 const Result = () => {
-   
+    const pdfExportComponent = React.useRef(null);
+
+    const exportPDF = () => {
+      pdfExportComponent.current.save();
+    };
 
       const currentDate = new Date();
       const dateString = currentDate.toLocaleDateString("en-US", { day: "2-digit", month: "2-digit", year: "numeric" });
@@ -24,6 +27,7 @@ console.log(timeString);
     <div className='result-cont'>
       <div className="headee">
         <center><img src={logo} alt="" className="unilogo" width='45px' height='45px' /></center>
+        <PDFExport ref={pdfExportComponent}>
         <h4 className="heading">
         NATIONAL OPEN UNIVERSITY OF NIGERIA <br />
         NOU191020817 GIDIGBI Ayomide Daniel <br />
@@ -31,7 +35,7 @@ console.log(timeString);
         Exam Results for 2022/2 Session
         </h4>
         <center id='searcht'>
-            <button className="pdf" onClick={generatePDF}>PDF</button>
+            <button className="pdf" onClick={exportPDF}>PDF</button>
             <span className="searche">
 
             
@@ -39,8 +43,17 @@ console.log(timeString);
             <input type="text"id='search'/>
             </span>
         </center>
+        <style>
+            {
+                `
+            h4{
+                color:green;
+            }`
+            }
+        </style>
+        </PDFExport>
       </div>
-    </div>
+ </div>
   )
 }
 
