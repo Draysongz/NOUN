cf5f0f12f3b33c6144ca678249b03fc851647046
Ackermann <dharmiegra4@gmail.com>
2023-02-08 19:28:35 +0100

    err

--- src/Err.jsx
+++ src/Err.jsx
@@ -4,8 +4,17 @@ import './Err.css'
 const Err = () => {
   return (
     <div>
-      <h1 className="whoops">Whoops!</h1>
-      <p className="some">Something went wrong</p>
+     <section id="not-found">
+    <div id="title">Simple Pure CSS3 &bull; 404 Error Page</div>
+    <div class="circles">
+      <p>404<br/>
+       <small>PAGE NOT FOUND</small>
+      </p>
+      <span class="circle big"></span>
+      <span class="circle med"></span>
+      <span class="circle small"></span>
+    </div>
+  </section>
     </div>
   )
 }
